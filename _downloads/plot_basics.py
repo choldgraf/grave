4fa187a8f81d1858a3e82412658970a000ada385
Chris Holdgraf <choldgraf@berkeley.edu>
2018-03-29 14:23:20 -0700

    Update documentation

---
+++ _downloads/plot_basics.py
@@ -0,0 +1,16 @@
+"""
+GraVE Documentation
+-------------------
+
+Test
+"""
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from grave import grave
+
+
+graph = nx.barbell_graph(10, 14)
+
+fig, ax = plt.subplots()
+grave.plot_network(graph, axes=ax)
